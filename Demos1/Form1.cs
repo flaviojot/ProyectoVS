e3f96db1ff9f3bfe921f641ae1a450cbcee5c43d
Flavio Jr <flaviojot@gmail.com>
2026-04-04 18:49:40 -0500

    agregado el boton

--- Demos1/Form1.cs
+++ Demos1/Form1.cs
@@ -11,5 +11,16 @@ namespace Demos1
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int numero1 = 15;
+            int numero2 = 5;
+            int total = numero1 + numero2;
+
+            textBox1.Text = total.ToString();
+
+        }
     }
 }
